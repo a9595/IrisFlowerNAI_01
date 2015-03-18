65793ef57d4904593fc19f9d0e66c94aff18d362
Andrii Kovalchuk <tieorange@gmail.com>
2015-03-18 22:26:49 +0100

    add numpy

--- funcs.py
+++ funcs.py
@@ -1,8 +1,9 @@
-
-
 def calc_distance(data_arr, user_arr):
     sum_of_arrays = 0
     for (data, user) in zip(data_arr, user_arr):
         sum_of_arrays += (data - user) ** 2
         # print("a: ", data, "; b: ", user)
-    return sum_of_arrays**(1/2)
+    return sum_of_arrays ** (1 / 2)
+
+    # print(trainingDataArr[:, :4])  # get all beside last
+    # print(trainingDataArr[:, -1])  # get last column
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# import numpy as np
+import numpy as np
 from funcs import calc_distance
 
-
-trainingDataArr = [[5.1, 3.5, 1.4, 0.2, 0],
-                   [4.9, 3.0, 1.4, 0.2, 0],
-                   [4.7, 3.2, 1.3, 0.2, 0],
-                   [4.6, 3.1, 1.5, 0.2, 0]]
+# init vars
+trainingDataArr = np.array([[5.1, 3.5, 1.4, 0.2, 0],
+                            [4.9, 3.0, 1.4, 0.2, 0],
+                            [4.7, 3.2, 1.3, 0.2, 0],
+                            [4.6, 3.1, 1.5, 0.2, 0]])
 
 trainingDataArrGood = [[5.1, 3.5, 1.4, 0.2],
                        [4.9, 3.0, 1.4, 0.2],
@@ -13,8 +13,12 @@ trainingDataArrGood = [[5.1, 3.5, 1.4, 0.2],
                        [4.6, 3.1, 1.5, 0.2]]
 
 userInputArr = [1, 2, 3, 4]  # TODO: user input
-
 distances_arr = []
+
+
+# let's play bro
+
+print(trainingDataArr[:, -1])  # get last column
 
 for idx, val in enumerate(trainingDataArrGood):
     print("#", idx, " distance = ", calc_distance(val, userInputArr))
