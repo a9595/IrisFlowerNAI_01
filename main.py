7018133dc09983bf089a53a31d54dc74e81b4baa
Andrii Kovalchuk <tieorange@gmail.com>
2015-03-18 21:49:43 +0100

    todo fix

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ trainingDataArrGood = [[5.1, 3.5, 1.4, 0.2],
                        [4.7, 3.2, 1.3, 0.2],
                        [4.6, 3.1, 1.5, 0.2]]
 
-userInputArr = [1, 2, 3, 4]  # T ODO: user input
+userInputArr = [1, 2, 3, 4]  # TODO: user input
 
 distances_arr = []
 
