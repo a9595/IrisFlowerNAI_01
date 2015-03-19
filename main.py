7d96decdbf791c89fc3dd9e81e2543d6be00c333
Andrii Kovalchuk <tieorange@gmail.com>
2015-03-19 14:26:27 +0100

    dictionary with distance and type of flower

--- main.py
+++ main.py
@@ -1,35 +1,30 @@
-import numpy as np
+from blaze.compute import numpy
 from funcs import calc_distance
-
+from funcs import get_training_data
 # init vars
-trainingDataArr = np.array([[5.1, 3.5, 1.4, 0.2, 0],
-                            [4.9, 3.0, 1.4, 0.2, 0],
-                            [4.7, 3.2, 1.3, 0.2, 0],
-                            [4.6, 3.1, 1.5, 0.2, 0]])
+trainingDataArr = get_training_data()
 
-trainingDataArrGood = [[5.1, 3.5, 1.4, 0.2],
-                       [4.9, 3.0, 1.4, 0.2],
-                       [4.7, 3.2, 1.3, 0.2],
-                       [4.6, 3.1, 1.5, 0.2]]
-
+trainingDataArrGood = trainingDataArr[:, :4]
+trainingDataLastColumnArr = trainingDataArr[:, -1]
 userInputArr = [1, 2, 3, 4]  # TODO: user input
 distances_arr = []
 
+dictionaryData = {}
 
 # let's play bro
-
-print(trainingDataArr[:, -1])  # get last column
-
 distance_rez = 0
 for idx, val in enumerate(trainingDataArrGood):
-    distance_rez = calc_distance(val, userInputArr)
-    print("#", idx, " distance = ", distance_rez)
+    distance_rez = calc_distance(val, userInputArr) # calc distance
+    flower_type = trainingDataLastColumnArr[idx] # get type of flower
+    # print("#", idx, " = ", distance_rez, " (", flower_type, ")")
     distances_arr.append(distance_rez)
+    dictionaryData[distance_rez] = flower_type
 
-print("distance_arr =  ", distances_arr)
+print(dictionaryData)
 
-flower_arr = np.append(trainingDataArr[:, -1], distances_arr, 1)
-# flower_arr.shape
-print("flower_arr = ", flower_arr)
+
+
+# distances_arr.sort()
+# print("SORTED arr  ", distances_arr)
 
 
